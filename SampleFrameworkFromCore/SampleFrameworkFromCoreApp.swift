9f6b2ea7ee5a25f2d5f8596295121b06bea1f998
Volodymyr Chekyrta <volodymyr.chekyrta@raccoongang.com>
2024-07-09 22:22:46 +0300

    Initial Commit

---
+++ SampleFrameworkFromCore/SampleFrameworkFromCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFrameworkFromCoreApp.swift
+//  SampleFrameworkFromCore
+//
+//  Created by Volodymyr Chekyrta on 9.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SampleFrameworkFromCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
